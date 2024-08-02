48f7bfc80fa510301283e1793cd31f5e4dcf88b1
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-08-02 17:33:04 +0300

    + db connection

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,13 @@
 "use strict";
+import "dotenv/config";
 import express from 'express';
+import connectDB from './config/db';
 
 const app = express();
 
 const PORT = 3000;
+
+connectDB();
 
 app.use('/', ()=>{})
 
