c6dc3c7f32eac8aec0e45708e53ab595f4eea787
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-08-02 17:23:22 +0300

    + empty express app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,12 @@
 "use strict";
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+app.use('/', ()=>{})
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`)
+})
