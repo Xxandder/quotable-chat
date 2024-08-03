d7bb7988a2b0acafedc045e6b80f3b4c82cd8b20
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-08-03 21:46:19 +0300

    + linter and prettifier for frontend

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,18 +1,26 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
-}
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'redux-saga'],
+  rules: {
+    
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'semi': ['off'],
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": "off",
+    "padded-blocks": "off",
+    "import/no-extraneous-dependencies": "off"
+  },
+};
