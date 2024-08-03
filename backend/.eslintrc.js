ffc7b28fcac131c42c73d6f7767cf1512e5cb7cb
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-08-03 18:30:33 +0300

    + eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint", "prettier"],
+    overrides: [
+      {
+        // files: ["*.ts", "*.tsx"],
+        files: ["src/**/*.ts", "src/**/*.js", "test/**/*.ts"],
+        extends: [
+          "airbnb-base",
+          "airbnb-typescript/base",
+          "prettier",
+          "plugin:@typescript-eslint/eslint-recommended",
+          "plugin:@typescript-eslint/recommended",
+        ],
+  
+        parserOptions: {
+          parser: "@typescript-eslint/parser",
+          project: "tsconfig.json",
+          sourceType: "module",
+        },
+      },
+    ],
+  };
