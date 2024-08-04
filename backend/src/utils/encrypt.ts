051edafac7e5ddb38c57c72d74bb878089a7d526
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-08-04 12:55:17 +0300

    + encrypt service

---
+++ backend/src/utils/encrypt.ts
@@ -0,0 +1,56 @@
+import bcrypt from "bcrypt";
+
+interface IEncryptService {
+  hashPassword(password: string): Promise<string>;
+  comparePasswords(password: string, hashedPassword: string): Promise<boolean>;
+}
+
+export class EncryptService implements IEncryptService {
+  private bcryptService: typeof bcrypt;
+
+  private saltRounds: number;
+
+  constructor(bcryptService: typeof bcrypt, saltRounds: number) {
+    this.saltRounds = saltRounds;
+    this.bcryptService = bcryptService;
+  }
+
+  public async hashPassword(password: string): Promise<string> {
+    try {
+      const hashedPassword = await this.bcryptService.hash(
+        password,
+        this.saltRounds,
+      );
+      return hashedPassword;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`Error with hashing password: ' ${error.message}`);
+      } else {
+        throw new Error("Unknown error with hashing password:");
+      }
+    }
+  }
+
+  public async comparePasswords(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    try {
+      const isMatch = await this.bcryptService.compare(
+        password,
+        hashedPassword,
+      );
+      return isMatch;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        throw new Error(`Error with comparing passwords: ${error.message}`);
+      } else {
+        throw new Error("Unknown error with comparing passwords:");
+      }
+    }
+  }
+}
+
+const encryptService = new EncryptService(bcrypt, process.env.SALT_ROUNDS);
+
+export default encryptService;
