97983b5d332d69c3f57d14e1ba7e7e5f4dabc7b1
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-08-05 01:17:59 +0300

    + user router for registration

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import passport from "passport";
+import { userController } from "../controllers/user.controller";
 
 const router: Router = Router();
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
     res.json(req.user)
 })
+
+router.post('/register', userController.register)
+
 
 export { router as userRouter };
