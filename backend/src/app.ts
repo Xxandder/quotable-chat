f1380ab563fd85fe7b8f0aa6828373db5fc84bec
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-08-04 22:13:11 +0300

    + connected passport strategy for app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,9 @@ import connectDB from "./config/db";
 import { requestLogger } from "./middleware/request-logger.middleware";
 import { loggerService } from "./utils/logger";
 
+import { router } from "./routes/routes";
+
+import { initPassport } from "./config/passport";
 
 const app = express();
 
@@ -12,10 +15,12 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(session({
-  secret: process.env?.SESSION_SECRET,
+  secret: process.env?.SESSION_SECRET as string,
   resave: false,
   saveUninitialized: false
 }));
+
+initPassport(app)
 
 const PORT = 3000;
 
@@ -23,7 +28,7 @@ connectDB();
 
 app.use(requestLogger);
 
-app.use("/", () => {});
+app.use(router)
 
 app.listen(PORT, () => {
   loggerService.info(`Listening on port ${PORT}`);
--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -1,49 +1,58 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
+import { Express } from "express";
 import { User } from '../models/user.model';
 import encryptService from '../utils/encrypt';
 import { UserEntity } from '../types/user-entity.type';
 
-passport.use(new LocalStrategy({ usernameField: "email"},
-  async (email: string, password: string, done) => {
-    try {
-      const user = await User.findOne({ email });
+function initPassport(app: Express) {
+    app.use(passport.initialize());
+    app.use(passport.authenticate('session'));
 
+    passport.use(new LocalStrategy({ usernameField: "email"},
+        async (email: string, password: string, done) => {
+          try {
+            const user = await User.findOne({ email });
       
-      if (!user) {
-        return done(null, false, { message: 'User not found' });
-      }
+            
+            if (!user) {
+              return done(null, false, { message: 'User not found' });
+            }
+      
+            const userEntity: UserEntity = {
+              id: user.id,
+              createdAt: user.createdAt,
+              updatedAt: user.updatedAt,
+              email: user.email,
+              firstName: user.firstName,
+              lastName: user.lastName,
+              passwordHash: user.passwordHash,
+              avatarUrl: user.avatarUrl,
+            };
+           
+            if (await encryptService.comparePasswords(password, user.passwordHash as string)) {
+              return done(null, false, { message: 'Incorrect password' });
+            }
+            return done(null, userEntity);
+          } catch (error) {
+            return done(error);
+          }
+        }
+      ));
+      
+      passport.serializeUser<UserEntity>((user, done) => {
+          done(null, user as UserEntity);
+      });
+      
+      passport.deserializeUser(async (id: string, done) => {
+        try {
+          const user = await User.findById(id); 
+          done(null, user);
+        } catch (error) {
+          done(error, null);
+        }
+      });
+}
 
-      const userEntity: UserEntity = {
-        id: user.id,
-        createdAt: user.createdAt,
-        updatedAt: user.updatedAt,
-        email: user.email,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        passwordHash: user.passwordHash,
-        avatarUrl: user.avatarUrl,
-      };
-     
-      if (await encryptService.comparePasswords(password, user.passwordHash as string)) {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-      return done(null, userEntity);
-    } catch (error) {
-      return done(error);
-    }
-  }
-));
 
-passport.serializeUser<UserEntity>((user, done) => {
-    done(null, user as UserEntity);
-});
-
-passport.deserializeUser(async (id: string, done) => {
-  try {
-    const user = await User.findById(id); 
-    done(null, user);
-  } catch (error) {
-    done(error, null);
-  }
-});
+export { initPassport };
