1e66e4c3747ff287163d9f197ec69b43df218f4b
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-08-04 13:42:13 +0300

    + expres ssessions for app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,21 @@
 import "dotenv/config";
 import express from "express";
+import session from "express-session";
 import connectDB from "./config/db";
 import { requestLogger } from "./middleware/request-logger.middleware";
 import { loggerService } from "./utils/logger";
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.use(session({
+  secret: process.env?.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 
 const PORT = 3000;
 
--- backend/src/types/env.d.ts
+++ backend/src/types/env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     MONGO_URI: string;
     SALT_ROUNDS: number;
+    SESSION_SECRET: string;
   }
 }
