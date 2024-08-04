fb99180a32886248c42554dc2a9705f81c801941
Xxandder <aleksandr.chernovryvka@gmail.com>
2024-08-05 00:26:55 +0300

    * updated import

--- backend/src/middleware/request-logger.middleware.ts
+++ backend/src/middleware/request-logger.middleware.ts
@@ -1,10 +1,8 @@
 import { Request, Response, NextFunction } from "express";
-import { LoggerService } from "../utils/logger";
-
-const logger = new LoggerService();
+import { loggerService } from "../utils/logger";
 
 const requestLogger = (req: Request, res: Response, next: NextFunction) => {
-  logger.info("Incoming request", { method: req.method, url: req.url });
+  loggerService.info("Incoming request", { method: req.method, url: req.url });
   next();
 };
 
